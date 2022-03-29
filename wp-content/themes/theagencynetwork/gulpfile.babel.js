76f2c2ac627d82d74f07661498ea0ee0b9175b95
Max <maxsevostianov@192.168.1.3>
2022-03-29 13:04:33 -0400

    theagencytheme with gulp

--- wp-content/themes/theagencynetwork/gulpfile.babel.js
+++ wp-content/themes/theagencynetwork/gulpfile.babel.js
@@ -1,17 +1,83 @@
+/* INSTRUCTIONS ARE HERE: https://css-tricks.com/gulp-for-wordpress-creating-the-tasks/#top-of-site */
 import gulp from 'gulp';
 
-import { src, dest } from 'gulp';
+import webpack from 'webpack-stream';
+
+import { src, dest, watch, series, parallel } from 'gulp';
 import yargs from 'yargs';
-/*import sass from 'gulp-sass';*/
+
+/*import sass from 'gulp-sass';*/ /* <<-- this did not work, had to do [npm install node-sass]*/
+const sass = require('gulp-sass')(require('sass'));
+
 import cleanCss from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 const PRODUCTION = yargs.argv.prod;
 
-const sass = require('gulp-sass')(require('sass'));
+import postcss from 'gulp-postcss';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'autoprefixer';
+import del from 'del';
+import named from 'vinyl-named';
 
 export const styles = () => {
   return src('src/scss/bundle.scss')
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulpif(PRODUCTION, postcss([ autoprefixer ])))
     .pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
     .pipe(dest('dist/css'));
 }
+
+export const copy = () => {
+  return src(['src/**/*','!src/{images,js,scss}','!src/{images,js,scss}/**/*'])
+    .pipe(dest('dist'));
+}
+
+export const clean = () => del(['dist']);
+
+export const watchForChanges = () => {
+  watch('src/scss/**/*.scss', styles);
+  watch(['src/**/*','!src/{js,scss}','!src/{js,scss}/**/*'], copy);
+  watch('src/js/**/*.js', scripts);
+}
+
+export const scripts = () => {
+  return src(['src/js/bundle.js'])
+    .pipe(named())
+    .pipe(webpack({
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      },
+      mode: PRODUCTION ? 'production' : 'development',
+      devtool: !PRODUCTION ? 'inline-source-map' : false,
+      output: {
+        filename: '[name].js'
+      },
+    }))
+    .pipe(dest('dist/js'));
+}
+
+export const dev = series(clean, parallel(styles, copy, scripts), watchForChanges);
+export const build = series(clean, parallel(styles, copy, scripts));
+export default dev;
+
+/*
+export const images = () => {
+  return src('src/images/!**!/!*.{jpg,jpeg,png,svg,gif}')
+    .pipe(gulpif(PRODUCTION, imagemin()))
+    .pipe(dest('dist/images'));
+}
+*/
+
+
